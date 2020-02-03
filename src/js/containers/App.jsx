bfedd9320ca77c3c8d7f3e62101a30b5f7ca2240
Mi-lex <mishin-lex@mail.ru>
2020-02-03 18:14:48 +0300

    make error message component

--- src/js/containers/App.jsx
+++ src/js/containers/App.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import RegisterUserForm from './RegisterUserForm';
+import ErrorMessage from '../components/ErrorMessage';
 
-export default function App() {
+const App = ({ error }) => {
 	return (
 		<div className="page-wrapper flex">
 			<div className="page-container">
 				<RegisterUserForm />
 			</div>
+			{error && <ErrorMessage className="error">{error}</ErrorMessage>}
 		</div>
 	);
-}
+};
+
+App.propTypes = {
+	error: PropTypes.string,
+};
+
+App.defaultProps = {
+	error: null,
+};
+
+const mapStateToProps = (state) => {
+	const { error } = state.users;
+
+	return {
+		error,
+	};
+};
+
+export default connect(mapStateToProps)(App);
